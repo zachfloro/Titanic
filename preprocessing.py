218cbb4ab350f0328cc1f49f551335df1cee0526
zachfloro <zachfloro13@gmail.com>
2020-09-21 20:21:20 -0400

    finished feat_eng and scale_feats functions in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,42 @@
 import pandas as pd
-
-df = pd.read_csv('Data/train.csv')
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
 
 def feat_eng(df):
     '''
     Parameters: 
         df - a dataframe with features for the titanic dataset from Kaggle expected columns are ('Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked')
     Returns:
-        df_eng - the transformed dataframe ready for machine learning
+        df - the transformed dataframe ready for machine learning
     '''
+    
+    # Drop Name, Ticket & Cabin columns
+    df.drop(columns=['Name', 'Ticket', 'Cabin'], inplace=True)
+    
+    # Create boolean columns for sex and Embarked
+    dummies = pd.get_dummies(df[['Sex', 'Embarked']])
+    df.drop(columns=['Sex', 'Embarked'],inplace=True)
+    df = df.merge(dummies, how='inner', left_index=True, right_index=True)
+    
+    # Fill missing values in Age using Median
+    imp = SimpleImputer(missing_values=np.nan, strategy='median')
+    Age = np.array(df['Age'])
+    Age = imp.fit_transform(Age.reshape(-1,1))
+    df['Age'] = Age
+    
+    return df
+
+def scale_feats(df):
+    '''
+    Parameters:
+        df - a dataframe that has already been engineered by feat_eng
+    Returns:
+        df_scaled - A dataframe that is normalized
+    '''
+    scaler = MinMaxScaler()
+    scaler.fit(df)
+    df_scaled = pd.DataFrame(scaler.transform(df),columns=df.columns)
+    
+    return df_scaled
+        
